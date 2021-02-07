8d7793431d4b43cd428594d399d407ef59d305fd
hotsuitor <hotsuitor@qq.com>
2021-02-07 18:09:39 +0800

    post headers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -10,16 +11,22 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
-  let { url, params } = config
+  const { url, params } = config
   return buildURL(url, params)
 }
 
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
