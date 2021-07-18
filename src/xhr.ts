dbedf95c1d698af1f41e45b7b6c939f13c87a66d
hotsuitor <hotsuitor@qq.com>
2021-07-18 18:39:08 +0800

    6.* 增强错误处理类型

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,5 +1,7 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
+
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
     const { data = null, url, method = 'get', headers, responseType, timeout } = config
@@ -44,18 +46,26 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (response.status >= 200 && response.status < 300) {
         resolve(response)
       } else {
-        reject(new Error(`Request failed with status code ${response.status}`))
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
       }
     }
 
     // 处理错误
-    request.onerror = function handleError(err) {
-      reject(new Error('Network Error ' + err))
+    request.onerror = function handleError() {
+      reject(createError('Network Error ', config, null, request))
     }
 
     // 处理请求超时
     request.ontimeout = function handleTimeout() {
-      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
     }
 
     Object.keys(headers).forEach(name => {
