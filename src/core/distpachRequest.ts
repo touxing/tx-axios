99c00519d064db07077d9b25586d296e5ae9482d
hotsuitor <hotsuitor@qq.com>
2021-07-18 20:08:38 +0800

    WIP: 接口扩展

---
+++ src/core/distpachRequest.ts
@@ -0,0 +1,82 @@
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
+import { createError } from '../helpers/error'
+
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
+
+    const request = new XMLHttpRequest()
+
+    if (responseType) {
+      request.responseType = responseType
+    }
+    if (timeout) {
+      request.timeout = timeout
+    }
+
+    // 新建ajax请求
+    request.open(method.toUpperCase(), url!, true)
+
+    // 请求完成回调
+    request.onreadystatechange = function handleLoad() {
+      // 请求状态不成功
+      if (request.readyState !== 4) {
+        return
+      }
+      // 网络错误或超时
+      if (request.status === 0) {
+        return
+      }
+
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      handleResponse(response)
+    }
+
+    function handleResponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
+    }
+
+    // 处理错误
+    request.onerror = function handleError() {
+      reject(createError('Network Error ', config, null, request))
+    }
+
+    // 处理请求超时
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    // 发起ajax请求
+    request.send(data)
+  })
+}
